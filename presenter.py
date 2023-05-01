cbb6b36ed4f60d77a799a941ffa70192f7f8440f
harshraj2717 <harshraj2717@gmail.com>
2023-05-02 01:32:34 +0530

    add drawing gestures + refactor and comment some code

--- presenter.py
+++ presenter.py
@@ -3,16 +3,14 @@ import cv2
 import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 
-# Variables
-slides = []
-cur_slide = 0
-allow_slide_change = True
-
 
 def start_presenting(Slide_directory_name: str) -> None:
-    global slides
-    global cur_slide
-    global allow_slide_change
+    # Local Variables
+    slides = []
+    cur_slide = 0
+    allow_slide_change_gesture = True
+    drawings = []
+    drawing_continuation = False
 
     # Collecting the slides in the slide directory
     # and sorting based on length also to prevent "10.*" coming after "1.*" instead of "2.*"
@@ -58,7 +56,7 @@ def start_presenting(Slide_directory_name: str) -> None:
 
             # Finding index finger and constraining it to half of the slides
             index_finger_x = int(np.interp(landmarks_list[8][0], [int(
-                cur_video_img.shape[1]) // 2, int(cur_video_img.shape[1])], [0, int(cur_slide_image.shape[1])]))
+                cur_video_img.shape[1]) // 2 + 100, int(cur_video_img.shape[1]) - 100], [0, int(cur_slide_image.shape[1])]))
             index_finger_y = int(np.interp(landmarks_list[8][1], [
                                  150, int(cur_video_img.shape[0])-150], [0, int(cur_slide_image.shape[0])]))
 
@@ -66,23 +64,55 @@ def start_presenting(Slide_directory_name: str) -> None:
             fingers = hand_detector.fingersUp(hands)
 
             # Checking for threshold height and hand position
+            # for getures that need to done above threshold line
             if cur_hands_pos_y < threshold_height:
-                # Adding slide change gesture
+                # Adding slide_change gesture
                 if fingers == [1, 0, 0, 0, 0]:
-                    if cur_slide > 0 and allow_slide_change:
+                    if cur_slide > 0 and allow_slide_change_gesture:
                         cur_slide -= 1
-                        allow_slide_change = False
+                        allow_slide_change_gesture = False
+                        # Clearing out any drawings
+                        drawings = []
+                        drawing_continuation = False
                 elif fingers == [0, 0, 0, 0, 1]:
-                    if cur_slide < len(slides) - 1 and allow_slide_change:
+                    if cur_slide < len(slides) - 1 and allow_slide_change_gesture:
                         cur_slide += 1
-                        allow_slide_change = False
+                        allow_slide_change_gesture = False
+                        # Clearing out any drawings
+                        drawings = []
+                        drawing_continuation = False
                 else:
-                    allow_slide_change = True
+                    allow_slide_change_gesture = True
+
+                # Adding clear_drawings gesture
+                if fingers == [0, 1, 1, 1, 0]:
+                    drawings = []
+                    drawing_continuation = False
 
             # Adding pointer gesture
+            if fingers == [0, 1, 0, 0, 0]:
+                cv2.circle(cur_slide_image, (index_finger_x,
+                           index_finger_y), 15, (0, 0, 255), cv2.FILLED)
+
+            # Adding drawing gesture
             if fingers == [0, 1, 1, 0, 0]:
                 cv2.circle(cur_slide_image, (index_finger_x,
                            index_finger_y), 15, (0, 0, 255), cv2.FILLED)
+                # drawing_continuation defines if we are in the drawing same as previous drawing or a new drawing
+                # this is important so that different across the slide don't get joined without the choice of user
+                if drawing_continuation:
+                    drawings[-1].append((index_finger_x, index_finger_y))
+                else:
+                    drawings.append([(index_finger_x, index_finger_y)])
+                drawing_continuation = True
+            else:
+                drawing_continuation = False
+
+        # Adding all the current drawings to the current slide image
+        for drawing in drawings:
+            for i in range(1, len(drawing)):
+                cv2.line(cur_slide_image,
+                         drawing[i-1], drawing[i], (0, 0, 255), 10)
 
         # Placing presenter's video on top of slide
         presentation_slide_height, presentation_slide_width, _ = cur_slide_image.shape
